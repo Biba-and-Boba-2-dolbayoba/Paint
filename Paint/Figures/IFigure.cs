f464c329af2b788eb7586d9a6a5b7c09090074a6
Ситников Даниил <den747f@gmail.com>
2024-12-03 18:44:03 +0300

    interface curveline figure moveable textbox

--- Paint/Figures/IFigure.cs
+++ Paint/Figures/IFigure.cs
@@ -1,5 +1,14 @@
 ﻿namespace Paint.Figures;
 
 internal interface IFigure {
+    public Color LineColor { get; set; }
+    public int BrushSize{  get; set; }
+    public Color BackColor{ get; set; }
+    public bool IsFilling {  get; set; }
+    public void Draw(Graphics g);
+    public void Hide(Graphics g);
+    public void Dash(Graphics g);
+    public void DrawSelection(Graphics g);
+    public void ContainsPoint(Point point);
 
 }
---
+++ Paint/Figures/ITextBox.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paint.Figures;
+internal interface ITextBox {
+    public Font TextFont {  get; set; }
+    public string Text {  get; set; }
+}
