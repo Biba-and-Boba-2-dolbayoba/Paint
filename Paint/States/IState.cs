8678eb7902bd44e1b4011f00a88e7ba5d6a17e42
wnkbll <arzhevitin.g@gmail.com>
2024-12-03 23:24:40 +0500

    BBBBBBBBBBBBBBB

--- Paint/States/IState.cs
+++ Paint/States/IState.cs
@@ -1,7 +1,7 @@
 ﻿namespace Paint.States;
+
 internal interface IState {
     public void MouseUpHandler(object sender, MouseEventArgs e);
     public void MouseMoveHandler(object sender, MouseEventArgs e);
     public void MouseDownHandler(object sender, MouseEventArgs e);
-
 }
