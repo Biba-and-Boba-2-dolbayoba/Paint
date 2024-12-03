c5ba920f2e94a5d294f4e4fce5d77f9d2cb5945b
wnkbll <arzhevitin.g@gmail.com>
2024-12-03 22:22:06 +0500

    clean up code

--- Paint/States/DrawState.cs
+++ Paint/States/DrawState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Paint.States;
+﻿namespace Paint.States;
 internal class DrawState : IState {
     public void MouseDownHandler(object sender, MouseEventArgs e) {
         throw new NotImplementedException();
--- Paint/States/IState.cs
+++ Paint/States/IState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Paint.States;
+﻿namespace Paint.States;
 internal interface IState {
     public void MouseUpHandler(object sender, MouseEventArgs e);
     public void MouseMoveHandler(object sender, MouseEventArgs e);
