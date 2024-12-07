089d13bd2be8578154ad6acb3766f7cbb2f23d19
Ситников Даниил <den747f@gmail.com>
2024-12-07 17:12:28 +0300

    canvascreate update

--- Paint/UI/UiCreateCanvas.cs
+++ Paint/UI/UiCreateCanvas.cs
@@ -33,7 +33,32 @@ internal partial class UiCreateCanvas : Form {
     }
 
     private void CancelButtonClick(object sender, EventArgs e) {
+
+        this.CanvasSize = new Size(0, 0);
         this.DialogResult = DialogResult.Cancel;
         this.Close();
+    }
+
+    private void ManualSelectionCheckBoxCheckedChanged(object sender, EventArgs e) {
+        bool isChecked = this.ManualSelectionCheckBox.Checked;
+        this.WidthTextBox.Enabled = isChecked;
+        this.HeightTextBox.Enabled = isChecked;
+
+
+        if (!isChecked) {
+            this.WidthTextBox.Text = "320";
+            this.HeightTextBox.Text = "240";
+        }
+
+        this.SizeSmallButton.Enabled = !isChecked;
+        this.SizeAverageButton.Enabled = !isChecked;
+        this.SizeBigButton.Enabled = !isChecked;
+
+        
+        if (isChecked) {
+            this.SizeSmallButton.Checked = false;
+            this.SizeAverageButton.Checked = false;
+            this.SizeBigButton.Checked = false;
+        }
     }
 }
