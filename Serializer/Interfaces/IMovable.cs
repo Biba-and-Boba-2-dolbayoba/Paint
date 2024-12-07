58eab791a6c65ef64d9e93c559883f6296e66dfe
wnkbll <arzhevitin.g@gmail.com>
2024-12-07 12:37:47 +0500

    delete test project

--- Serializer/Interfaces/IMovable.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Drawing;
-
-namespace Serializer.Interfaces;
-internal interface IMovable : ITopBottomDepends {
-    public bool CanMove(int dx, int dy, Size canvasSize);
-    public void Move(int dx, int dy);
-}
